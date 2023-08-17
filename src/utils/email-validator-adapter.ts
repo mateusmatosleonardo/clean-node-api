702f3f0ee339148a8860c8a6d2996b86619aeaa8
mateus <m4teusmatos@gmail.com>
2023-08-16 23:24:04 -0300

    test: comment

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -4,5 +4,6 @@ import validator from 'validator';
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
     return validator.isEmail(email);
+    // ! todo
   }
 }
